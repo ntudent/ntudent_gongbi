248e293a5f3dffc25dfc03ee599425c7dc9f0e27
ntudent <71329976+ntudent@users.noreply.github.com>
2022-07-04 22:22:29 +0800

    Update all.js

--- js/all.js
+++ js/all.js
@@ -1,6 +1,6 @@
-function getday_all(k) {
+function getday_all(k, day) {
     var someDate = new Date(k);
-    var numberOfDaysToAdd = 7;
+    var numberOfDaysToAdd = day;
     var result = someDate.setDate(someDate.getDate() + numberOfDaysToAdd);
     var g = new Date(result);
     if (g.getMonth()+1 < 10) {
@@ -25,7 +25,7 @@ function thisgb(gb_name, week) {
         return (gb_name + " Week " + week);
     }
     else {
-        return (gb_name + week);
+        return (gb_name + " " + week);
     }
 }
 function getTr(id, parentId) {
@@ -46,17 +46,22 @@ $(() => {
     item_all = new Array()
     gb_all = new Array()
     //改共筆改以下這段
-    item_all[0] = new Array(thisgb("OD", "1"), n[19], n[23], "2022-07-01", n[17], "") //(共筆, 寫手1, 寫手2, 寫手日, 審稿, 審稿日)
-    item_all[1] = new Array(thisgb("OD", "2"), n[1], n[19], "2022-07-02", n[10], "")
-    item_all[2] = new Array(thisgb("生理", "1"), n[2], n[9], "2022-06-30", n[19], "")
-    item_all[3] = new Array(thisgb("口解", "考古"), n[17], n[0], "2022-07-01", n[0], "x")
-    item_all[4] = new Array(thisgb("醫人", "1"), n[3], n[19], "2022-06-26", n[8], "")
-    item_all[5] = new Array(thisgb("醫人", "2"), n[21], n[19], "2022-06-30", n[7], "")
-    item_all[6] = new Array(thisgb("醫人", "3"), n[4], n[19], "2022-07-01", n[26], "")
-    item_all[7] = new Array(thisgb("醫人", "0"), n[5], n[6], "2022-06-25", n[19], "")
-    item_all[8] = new Array(thisgb("胚胎", "0"), n[33], n[34], "2022-07-04", n[19], "")
+    item_all[0] = new Array(thisgb("OD", "1"), n[19], n[23], "2022-07-05", "", n[17], "") //(共筆, 寫手1, 寫手2, 寫手日, 交稿日, 審稿, 審稿日)
+    item_all[1] = new Array(thisgb("OD", "2"), n[1], n[19], "2022-07-05", "", n[10], "")
+    item_all[2] = new Array(thisgb("生理", "1"), n[2], n[9], "2022-07-04", "", n[19], "")
+    item_all[3] = new Array(thisgb("口解", "考古"), n[17], n[0], "x", "2022-07-05", n[0], "x")
+    item_all[4] = new Array(thisgb("醫人", "1"), n[3], n[19], "2022-07-06", "", n[8], "")
+    item_all[5] = new Array(thisgb("醫人", "2"), n[21], n[19], "2022-07-02", "", n[7], "")
+    item_all[6] = new Array(thisgb("醫人", "3"), n[4], n[19], "2022-07-01", "", n[26], "")
+    item_all[7] = new Array(thisgb("醫人", "0"), n[5], n[6], "2022-07-03", "", n[19], "")
+    item_all[8] = new Array(thisgb("胚胎", "0"), n[33], n[34], "2022-07-04", "", n[19], "")
+    item_all[9] = new Array(thisgb("胚胎", "1"), n[33], n[34], "2022-07-05", "", n[19], "")
+    item_all[10] = new Array(thisgb("胚胎", "2"), n[33], n[34], "2022-07-06", "", n[19], "")
+    item_all[11] = new Array(thisgb("胚胎", "3"), n[33], n[34], "2022-07-07", "", n[19], "")
+    item_all[12] = new Array(thisgb("生理", "考古"), n[29], n[0], "x", "2022-07-04", n[0], "x")
+    item_all[13] = new Array(thisgb("生理", "考古古"), n[30], n[0], "x", "2022-07-15", n[0], "x")
     //改共筆改以上這段
-    
+
     $('#yourname').keypress(function(e) { //按enter鍵也觸發#enter_name的onclick()事件
         key = window.event ? e.keyCode : e.which
         if (key == 13) {
@@ -67,7 +72,7 @@ $(() => {
         sorting_layer_id = 1
         //刪既存編號數字id的tr
         //var del = document.querySelector('removable')
-        for (i=0; i<100; i++) {
+        for (i=0; i<100; i++) { //再按一次鍵後清空所有數字(id="1"~"99")的<tr>
             if (document.getElementById(i) !== null) {
                 //alert(document.getElementById(i))
                 document.getElementById(i).remove("#"+toString(i))
@@ -83,31 +88,58 @@ $(() => {
         }
         $('#yourname').val('') //清除<input>中字串
         $('#nameBlank').text(yourname) //放上姓名
-       
         for (i=0; i<n_time; i++) {
             gb_all[i] = new Array() //建新array元素
         }
-        for (i=0; i<n_time; i++) {
-            for (j=0; j<6; j++) {
-                if (j==3 || j==5) {
-                    if (j==5 && item_all[i][5] == "") {
-                        item_all[i][5] = getday_all(item_all[i][3]) //審稿日=期限日+7天
+        for (i=0; i<n_time; i++) { //改寫item_all[][]成gb_all[][]
+            for (j=0; j<7; j++) {
+                if (j == 3) {
+                    if (item_all[i][3] == "x") { //考古
+                        gb_all[i][3] = "" //考古無上課日
+                        continue;
+                    } else {
+                        dateParse = Date.parse(item_all[i][3] + " 00:00:01") //做期限日秒數轉換
+                        d = new Date()
+                        d = d.getTime() //當下毫秒
+                        count_d = (dateParse - d ) / (1000 * 60 * 60 * 24) //毫秒換秒
+                        count_d = Math.ceil(count_d) //剩餘日數(無條件進位)
+                        gb_all[i][3] = count_d
                     }
-                    else if (j==5 && item_all[i][5] == "x"){
-                        gb_all[i][5] = "" //考古無審稿日
-                        continue
+                } else if (j == 4) { //交稿日
+                    if (item_all[i][3] == "x") { //考古
+                        dateParse = Date.parse(item_all[i][4] + " 00:00:01") //做期限日秒數轉換
+                        d = new Date()
+                        d = d.getTime() //當下毫秒
+                        count_d = (dateParse - d ) / (1000 * 60 * 60 * 24) //毫秒換秒
+                        count_d = Math.ceil(count_d) //剩餘日數(無條件進位)
+                        gb_all[i][4] = count_d
+                    } else {
+                        item_all[i][4] = getday_all(item_all[i][3], 2) //其他人要從item_all[i][3]加2天
+                        dateParse = Date.parse(item_all[i][4] + " 00:00:01") //做期限日秒數轉換
+                        d = new Date()
+                        d = d.getTime() //當下毫秒
+                        count_d = (dateParse - d ) / (1000 * 60 * 60 * 24) //毫秒換秒
+                        count_d = Math.ceil(count_d) //剩餘日數(無條件進位)
+                        gb_all[i][4] = count_d
                     }
-                    dateParse = Date.parse(item_all[i][j] + " 00:00:01") //做期限日秒數轉換
-                    d = new Date()
-                    d = d.getTime() //當下毫秒
-                    count_d = (dateParse - d ) / (1000 * 60 * 60 * 24) //毫秒換秒
-                    count_d = Math.ceil(count_d) //剩餘日數(無條件進位)
-                    gb_all[i][j] = count_d 
+                } else if (j == 6) { //審稿日
+                    if (item_all[i][6] == "x") { //考古
+                        gb_all[i][6] = "" //考古無上課日
+                        continue;
+                    } else {
+                        item_all[i][6] = getday_all(item_all[i][3], 3) //其他人要從item_all[i][3]加3天
+                        dateParse = Date.parse(item_all[i][6] + " 00:00:01") //做期限日秒數轉換
+                        d = new Date()
+                        d = d.getTime() //當下毫秒
+                        count_d = (dateParse - d ) / (1000 * 60 * 60 * 24) //毫秒換秒
+                        count_d = Math.ceil(count_d) //剩餘日數(無條件進位)
+                        gb_all[i][6] = count_d 
+                    }
                 }
                 else {
                     gb_all[i][j] = item_all[i][j]
                 }
-            }
+            }        
         }
         
         //2D all陣列記錄待輸出值
@@ -117,28 +149,30 @@ $(() => {
         //sorting_layer_id = 1 //每層<tr>的id
         for (i=0; i<n_time; i++) { //all[][]記錄值
             if (item_all[i][1] == yourname || item_all[i][2] == yourname) {
-                all[num_all] = new Array
+                all[num_all] = new Array()
 
-                if (item_all[i][5] == "x") { //考古人士
-                    all[num_all] = new Array(item_all[i][0], "考古", "", item_all[i][3], gb_all[i][3])
+                if (item_all[i][3] == "x") { //考古人士
+                    all[num_all] = new Array(item_all[i][0], "考古", "", item_all[i][4], gb_all[i][4])
                 }
                 else if (item_all[i][1] == yourname) { //寫手人士 && 名字==item[j][1]
-                    all[num_all] = new Array(item_all[i][0], "寫手", item_all[i][2], item_all[i][3], gb_all[i][3])
+                    all[num_all++] = new Array(item_all[i][0], "寫手(課)", item_all[i][2], item_all[i][3], gb_all[i][3])
+                    all[num_all] = new Array(item_all[i][0], "寫手(交)", item_all[i][2], item_all[i][4], gb_all[i][4])
                 }
                 else { //寫手人士 && 名字==item[j][2]
-                    all[num_all] = new Array(item_all[i][0], "寫手", item_all[i][1], item_all[i][3], gb_all[i][3])
+                    all[num_all++] = new Array(item_all[i][0], "寫手(課)", item_all[i][1], item_all[i][3], gb_all[i][3])
+                    all[num_all] = new Array(item_all[i][0], "寫手(交)", item_all[i][1], item_all[i][4], gb_all[i][4])
                 }
                 num_all += 1 //計數
             }
-            else if (item_all[i][4] == yourname) { //審稿人士
-                all[num_all] = new Array(item_all[i][0], "審稿", "", item_all[i][5], gb_all[i][5])
+            else if (item_all[i][5] == yourname) { //審稿人士
+                all[num_all] = new Array(item_all[i][0], "審稿", "", item_all[i][6], gb_all[i][6])
                 num_all += 1 //計數
             }
         }
         all.sort((a, b) => a[4] - b[4])
         for (i=0; i<num_all; i++) {
             getTr(sorting_layer_id, 'print_all')
-            for (j=0; j<6; j++) { 
+            for (j=0; j<7; j++) { 
                 let $td = $('<td class="removable">') //創造元素
                 n_sorting_layer_id_num = $('#' + sorting_layer_id)
                 if (all[i][4] == 0) {
@@ -154,6 +188,9 @@ $(() => {
             }
             sorting_layer_id += 1
         }
+        
+        
+        
     
     })
 })
