ba8b300e9fe23d64a74c6ffc9b5ca12e8d33e7b6
ntudent <71329976+ntudent@users.noreply.github.com>
2022-08-11 23:40:52 +0800

    Update checkin.js

--- js/checkin.js
+++ js/checkin.js
@@ -1,10 +1,10 @@
 function getData() { //裡面放page name，在body可以onload某頁面，在讓各物件onclick分別切不同page
     //XML為原生物件，專門和伺服器連線
     var req = new XMLHttpRequest();
-    req.open("get", "https://www.bullshitgenerator.com/");
+    req.open("get", "https://www.google.com/search?q=0056&oq=0056");
     req.onload = function() { //load事件，偵測連線的狀態結束
         //連線完成
-        var content = document.getElementsByClassName("brz-cp-color8");
+        var content = document.getElementsByClassName("IsqQVc");
         content.innerHTML = this.responseText;
         document.getElementById("body_checkin").text(content);
     };
