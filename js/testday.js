831013b42e2e8c0dbea9551dfddb0b38263dfb37
ntudent <71329976+ntudent@users.noreply.github.com>
2022-08-31 09:19:24 +0800

    Update testday.js

--- js/testday.js
+++ js/testday.js
@@ -19,6 +19,10 @@ $(() => {
     item_testday[3] = new Array(count_item_testday("胚胎"), "期末", "2022-12-20", "")
     item_testday[4] = new Array(count_item_testday("大體"), "期中", "2022-10-12", "")
     item_testday[5] = new Array(count_item_testday("大體"), "期末", "2022-12-20", "")
+    item_testday[6] = new Array(count_item_testday("組織"), "Quiz 1", "2022-10-11", "")
+    item_testday[7] = new Array(count_item_testday("組織"), "期中", "2022-10-17", "")
+    item_testday[8] = new Array(count_item_testday("組織"), "Quiz 2", "2022-12-05", "")
+    item_testday[9] = new Array(count_item_testday("組織"), "期末", "2022-12-19", "")
     //改共筆改以上這段
 
     for (i=0; i<n_test; i++) { //剩餘日計算
