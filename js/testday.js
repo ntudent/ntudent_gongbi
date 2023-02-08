740d50dfba4fc5875c7b5ee70d44f91e1de82428
ntudent <71329976+ntudent@users.noreply.github.com>
2023-02-08 20:48:35 +0800

    Update testday.js

--- js/testday.js
+++ js/testday.js
@@ -13,22 +13,17 @@ $(() => {
     item_testday = document.createElement('item_testday') //開一個DOM節點
     item_testday = new Array()
     //改共筆改以下這段
-    item_testday[0] = new Array(count_item_testday("口解"), "期末", "2022-12-23", "") //(考科, 範圍, 日期, 剩餘日)
-    item_testday[1] = new Array(count_item_testday("口胚"), "期中", "2022-10-28", "")
-    item_testday[2] = new Array(count_item_testday("口胚"), "期末", "2022-12-23", "")
-    item_testday[3] = new Array(count_item_testday("胚胎"), "期末", "2022-12-20", "")
-    item_testday[4] = new Array(count_item_testday("大體"), "期中", "2022-10-12", "")
-    item_testday[5] = new Array(count_item_testday("大體"), "期末", "2022-12-20", "")
-    item_testday[6] = new Array(count_item_testday("組織"), "Quiz 1", "2022-10-11", "")
-    item_testday[7] = new Array(count_item_testday("組織"), "期中", "2022-10-17", "")
-    item_testday[8] = new Array(count_item_testday("組織"), "Quiz 2", "2022-12-05", "")
-    item_testday[9] = new Array(count_item_testday("組織"), "期末", "2022-12-19", "")
-    item_testday[10] = new Array(count_item_testday("OD"), "期中", "2022-10-26", "")
-    item_testday[11] = new Array(count_item_testday("OD"), "期末", "2022-12-21", "")
-    item_testday[12] = new Array(count_item_testday("生理"), "期中", "2022-10-31", "")
-    item_testday[13] = new Array(count_item_testday("生理"), "期末", "2022-12-19", "")
-    item_testday[14] = new Array(count_item_testday("醫人"), "期中", "2022-10-28", "")
-    item_testday[15] = new Array(count_item_testday("醫人"), "期末", "2022-12-23", "")
+    item_testday[0] = new Array(count_item_testday("口解"), "期末", "2023-06-05", "") //(考科, 範圍, 日期, 剩餘日)
+    item_testday[1] = new Array(count_item_testday("胚胎"), "期末", "2023-12-20", "")
+    item_testday[2] = new Array(count_item_testday("大體"), "期中", "2023-04-25", "")
+    item_testday[3] = new Array(count_item_testday("大體"), "期末", "2023-06-06", "")
+    item_testday[4] = new Array(count_item_testday("組織"), "Quiz", "2023-03-28", "")
+    item_testday[5] = new Array(count_item_testday("組織"), "期末", "2023-04-18", "")
+    item_testday[6] = new Array(count_item_testday("牙周"), "期中", "2023-04-18", "")
+    item_testday[7] = new Array(count_item_testday("牙周"), "期末", "2023-04-18", "")
+    item_testday[8] = new Array(count_item_testday("牙放"), "期末", "2023-04-18", "")
+    item_testday[9] = new Array(count_item_testday("咬合"), "期末", "2023-04-18", "")
+    item_testday[10] = new Array(count_item_testday("FIX"), "期末", "2023-04-18", "")
     //改共筆改以上這段
 
     for (i=0; i<n_test; i++) { //剩餘日計算
